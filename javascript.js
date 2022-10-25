f5ba24c5b56132d58a237488323d730ff9bdf8bb
Dorian Birch <206dorian@gmail.com>
2022-10-25 17:51:30 -0500

    playing with styling/favicon

---
+++ javascript.js
@@ -0,0 +1,14 @@
+const  numbers = [3, 4];
+
+numbers.push(5,6);
+
+// numbers.splice(2, 0, 'a', 'b')
+
+numbers.unshift(1,2);
+
+console.log(numbers.indexOf(1));
+console.log(numbers.includes(1));
+
+
+    
+console.log(numbers);
