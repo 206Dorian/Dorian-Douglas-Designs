1a227a11e0b8debaab51a9cd24b6f512be825a77
Dorian Birch <206dorian@gmail.com>
2022-10-31 18:35:48 -0500

    playing with functions

--- javascript.js
+++ javascript.js
@@ -1,16 +1,54 @@
-const  numbers = [3, 4];
+function calculateArea(width, height) {
 
-numbers.push(5,6);
+var area = width * height;
+return area;
+}
 
-numbers.splice(2, 0, 'a', 'b')
+var wallOne = calculateArea(3, 5);
+var wallTwo = calculateArea(8, 5);
 
-numbers.unshift(1,2);
+console.log(wallOne, wallTwo)
 
-console.log(numbers.indexOf(1));
-console.log(numbers.includes(1));
+function getSize(width, height, depth) {
+
+    let area = width * height;
+    let volume = width * height * depth;
+    let sizes = [area, volume];
+    return sizes;
+}
+
+var areaOne = getSize(5, 10, 5) [1];
+var volumeOne = getSize(3, 5, 3)
 
 
-    
-console.log(numbers);
+console.log(areaOne, volumeOne)
+
+
+// function haiGay() { 
+
+//     let greet = 'Hai Gay' ;
+//     let gay = name;
+// }
+//     console.log (greet, name)
+
+
+
+
+
+
+
+
+
+
+
+
+// const  numbers = [3, 4];
+// numbers.push(5,6);
+// numbers.splice(2, 0, 'a', 'b')
+// numbers.unshift(1,2);
+// console.log(numbers.indexOf(1));
+// console.log(numbers.includes(1));
+
+// console.log(numbers);
 
 
